6ea36e8e7aebf8ac2f27e0ffeb3b9f5888854441
Vadym Rybachenko <vrybachenko@playtika.com>
2020-03-09 21:10:54 +0200

    add next methods and tests: subtraction, division, square, root, exponentiating

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -12,19 +12,53 @@ public class Calculator {
         return a + b;
     }
 
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
     public int multiply(int a, int b) {
 
         return a * b;
     }
 
-    //TODO
-    //add methods for multiply, division, subtraction, square root, x^2
-
     public double division(double a, double b) {
 
-        return a / b;
+        double divisionResult = a / b;
+
+        if(Double.isInfinite(divisionResult)){
+            System.out.println("Can't be divided by 0");
+            divisionResult = 0;
+        }
+
+      return divisionResult;
     }
 
+    public int square(int a) {
+
+        return a * a;
+    }
+
+    public double root(double a) {
+
+        double rootResult = Math.sqrt(a);
+
+        if(Double.isNaN(rootResult)){
+            System.out.println("incorrect value: " + a);
+           rootResult = 0;
+        }
+
+        return rootResult;
+    }
+
+    public double exponentiating(double a, double b) {
+
+        //double exponentiatingResult
+
+        return Math.pow(a, b);
+    }
+
+    //TODO
+    //add methods for multiply, division, subtraction, square root, x^2
 }
 
 
--- src/test/java/AddTwoValuesTests.java
+++ src/test/java/AddTwoValuesTests.java
@@ -28,6 +28,46 @@ public class AddTwoValuesTests {
         assertEquals("-10 + -5 must be -15", -15, calc.add(-10, -5));
     }
 
+    @Test
+    public void testDivisionPositiveValues(){
+        assertEquals("4 / 2 must be 2", 2.0, calc.division(4,2));
+    }
+
+    @Test
+    public void testDivisionDivideByZero(){
+        assertEquals("4 / 0 must be Can't be divided by Zero", 0.0, calc.division(4,0));
+    }
+
+    @Test
+    public void testSubtractionPositiveValues(){
+        assertEquals("10 - 3 must be 7", 7, calc.subtraction(10,3));
+    }
+
+    @Test
+    public void testMultiplyPositiveValues() {
+        assertEquals("4 * 2 must be 8", 8, calc.multiply(4,2));
+    }
+
+    @Test
+    public void testSquarePositiveValues() {
+        assertEquals("8 in square must be 16", 16, calc.square(4));
+    }
+
+    @Test
+    public void testRootPositiveValues() {
+        assertEquals("square root of 9025 must be 95", 95.0, calc.root(9025));
+    }
+
+    @Test
+    public void testRootNegativeValues() {
+        assertEquals("square root of -2 must be NaN",0.0, calc.root(-2));
+    }
+
+    @Test
+    public void testExponentiatingPositiveValues() {
+        assertEquals("8 in the 3 degree is 512", 512.0, calc.exponentiating(8, 3));
+    }
+
     //TODO
     //add new tests for add() method
 
